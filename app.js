ca89ae035456481b13dd6af2a87ea158a094994f
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-05-20 10:48:44 +0100

    first stage complete

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+/* - Install our dependencies - npm i express
+- import recipes from recipes-data
+- import recipesRouter from recipes router
+- created app.use for the router path to access imported recipes.
+
+
+*/
+
 import express from "express";
 
 import { html } from "./config.js";
+
+import recipes from "./libs/recipes.js";
+import recipesRouter from "./router/recipes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use("/recipes", recipesRouter);
 
 /* DO NOT CHANGE THIS ROUTE - it serves our front-end */
 app.get("/", function (req, res) {
--- router/recipes.js
+++ router/recipes.js
@@ -1,3 +1,17 @@
-//Write your router code here!
+/* - Import recipes from recipes-data
+-create a variable to store express router function
+ - Create router handler
+ - export the recipesRouter
+ 
+*/
 
-module.exports = recipesRouter;
+import express from "express";
+import recipes from "../libs/recipes.js";
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    const responseArray = { success : true, message: `Here is your recipe`, payload: recipes };
+    res.json(responseArray)
+})
+
+export default router;
